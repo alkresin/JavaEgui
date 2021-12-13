90b3205e9a4f41c88223d8e9b8cbb6814eadbbba
Alexander S.Kresin <alkresin@yahoo.com>
2021-12-13 17:05:17 +0300

    build scripts updated

--- bld_egui_test.bat
+++ bld_egui_test.bat
@@ -1,5 +1,4 @@
 @echo off
 del test\*.class
-rem javac -sourcepath ./src -d bin src/alkresin/egui_test/Test1.java
 javac -classpath ./classes test/Test1.java
 rem jar cvf Test1.jar -C bin .
--- run_egui_test.bat
+++ run_egui_test.bat
@@ -1,3 +1,5 @@
 @echo off
 java -classpath classes;test Test1
+rem use javaw to get rid of console window:
+rem javaw -classpath classes;test Test1
 rem java -classpath egui.jar;test Test1
