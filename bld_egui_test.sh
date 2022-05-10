c69b4ee1f43315c885b85b7df39f384d99262bbb
alkresin <alkresin@yahoo.com>
2022-05-10 20:48:45 +0300

    Documentation started

--- bld_egui_test.sh
+++ bld_egui_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm ./test/*.class
-javac -classpath ./classes test/Test1.java
+#javac -classpath ./classes test/Test1.java
+javac -classpath ./classes test/$1.java
--- run_egui_test.sh
+++ run_egui_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #java -classpath ./bin alkresin.egui_test.Test1
-java -classpath classes:test Test1
+#java -classpath classes:test Test1
+java -classpath classes:test $1
 #java -classpath egui.jar:test Test1
